af8b1188eae67eba534fb682874f0093b4d1c6e7
devilran6 <3470826156@qq.com>
2024-04-12 15:55:02 +0800

    update

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,8 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    let a: Vec<i32> = (1..200).collect();
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(cnt: i32) -> i32 {
+    if cnt <= 40 {
+        2 * cnt
+    } else {
+        cnt
+    }
+}
 
 // Don't modify this function!
 #[test]
