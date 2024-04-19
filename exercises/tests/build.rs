a59c92b4642d8e08089761932f3158ee6eefcd0f
devilran6 <3470826156@qq.com>
2024-04-20 04:32:49 +0800

    update

--- exercises/tests/build.rs
+++ exercises/tests/build.rs
@@ -10,15 +10,17 @@ fn main() {
         .duration_since(std::time::UNIX_EPOCH)
         .unwrap()
         .as_secs(); // What's the use of this timestamp here?
-    let your_command = format!(
-        "Your command here with {}, please checkout exercises/tests/build.rs",
-        timestamp
-    );
-    println!("cargo:{}", your_command);
 
+    // let your_command = format!(
+    //     "Your command here with {}, please checkout exercises/tests/build.rs",
+    //     timestamp
+    // );
+    // println!("cargo:{}", your_command);
+    println!("cargo:rustc-env=TEST_FOO={}", timestamp);
     // In tests8, we should enable "pass" feature to make the
     // testcase return early. Fill in the command to tell
     // Cargo about that.
-    let your_command = "Your command here, please checkout exercises/tests/build.rs";
-    println!("cargo:{}", your_command);
+    // let your_command = "Your command here, please checkout exercises/tests/build.rs";
+    // println!("cargo:{}", your_command);
+    println!("cargo:rustc-cfg=feature=\"pass\"");
 }
--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -27,16 +27,16 @@
 //
 // You should NOT modify any existing code except for adding two lines of attributes.
 
-// I AM NOT DONE
-
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
 mod Foo {
     // No `extern` equals `extern "Rust"`.
-    fn my_demo_function(a: u32) -> u32 {
+    #[no_mangle]
+    pub extern "C" fn my_demo_function(a: u32) -> u32 {
         a
     }
 }
